b14744b39921b9b2867ccf520ea7407a550eb930
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-11 16:27:20 +0530

    added surpriseMe and handle change function with a default img and 2 button for generate and share this repo

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import {useNavigate} from 'react-router-dom'
-import { FormField } from '../components';
+import { FormField, Loader } from '../components';
+import {preview} from '../assets/index.js'
+import { getRandomPrompt } from '../utils/index.js';
+
 const CreatePost = () => {
  const navigate= useNavigate();
- const [form, setform] = useState({
+ const [form, setForm] = useState({
   name:'',
   prompt:'',
   photo:''
@@ -13,8 +16,14 @@ const CreatePost = () => {
  const[loading,setLoading]=useState(false);
 
  const handleSubmit=()=>{}
- const handleChange=()=>{}
- const handleSurpriseMe=()=>{}
+ const handleChange=(e)=>{
+  setForm({...form,[e.target.name]:e.target.value})
+ }
+ const handleSurpriseMe=()=>{
+  const randomPrompt=getRandomPrompt(form.prompt);
+  setForm({...form,prompt:randomPrompt})
+ }
+ const generateImage=()=>{}
   return (
     <section className='max-w-7xl mx-auto'>
         <div>
@@ -43,7 +52,47 @@ const CreatePost = () => {
               isSurpriseMe
               handleSurpriseMe={handleSurpriseMe}
               />
+
+              <div className='relative bg-gray-50 border brder-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center '>
+                {form.photo?(
+                  <img 
+                  src={form.photo}
+                  alt={form.prompt}
+                  className='w-full h-full object-contain'
+                  />
+                ):(
+                  <img 
+                  src={preview}
+                  alt='preview'
+                  className='w-9/12 h-9/12 object-contain opacity-40'
+                  />
+                )}
+
+                {generatingImage && (
+                  <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg '>
+                    <Loader/>
+                  </div>
+                )}
+              </div>
           </div>
+        <div className='mt-5 flex gap-5'>
+                <button
+                type='button'
+                onClick={generateImage}
+                className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+                >
+                  {generatingImage ?'Generating...':'Generate' }
+                </button>
+        </div>
+        <div className="mt-10">
+          <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you can share it with community </p>
+          <button 
+          type='submit'
+          className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {loading?'Sharing...':'Share With Community'}
+          </button>
+        </div>
         </form>
     </section>
   )
