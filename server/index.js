f2a1eda85472e3e8fcf2163ebbe5ea0c1f07f8c0
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-13 14:28:25 +0530

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors'
+import connectDb from './mongodb/connect.js';
 
 dotenv.config();
 
@@ -8,9 +9,13 @@ const app=express();
 app.use(cors());
 app.use(express.json({limit:'50mb'}));
 
+
+
 app.get('/',async(req,res)=>{
     res.send('Express Running ')
 })
+
+connectDb();
 
 const startServer=()=>{
     app.listen(8080,()=>
